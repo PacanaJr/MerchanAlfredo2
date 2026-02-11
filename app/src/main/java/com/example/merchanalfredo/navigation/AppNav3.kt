10bd9cfdcae648d616173f8ee017f06e28ab3d75
Alfredo <alfredomrchn@gmail.com>
2026-02-11 17:04:54 +0100

    inicio

---
+++ app/src/main/java/com/example/merchanalfredo/navigation/AppNav3.kt
@@ -0,0 +1,7 @@
+package com.example.merchanalfredo.navigation
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNav3() {
+    GestionNavegacion()
+}
---
+++ app/src/main/java/com/example/merchanalfredo/ui/PantLogin.kt
@@ -0,0 +1,27 @@
+package com.example.merchanalfredo.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.mitienda.viewmodel.AuthViewModel
+
+@Composable
+fun PantLogin(
+    onLoginOk: () -> Unit,
+    onRegistro: () -> Unit,
+    authViewModel: AuthViewModel
+) {
+   }
